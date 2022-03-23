ab1e829750a65a11368f885cf416dfde5563300c
Jessie Lesbian <jessielesbian@protonmail.com>
2022-03-23 11:19:44 +0700

    Fix SQL access errors

---
+++ OpenCEX.NET/DBManager.cs
@@ -0,0 +1,142 @@
+using MySql.Data.MySqlClient;
+using System;
+
+namespace jessielesbian.OpenCEX{
+	public sealed class SQLCommandFactory : IDisposable
+	{
+		private readonly MySqlConnection mySqlConnection;
+		private MySqlTransaction mySqlTransaction;
+		private bool disposedValue;
+		private MySqlDataReader dataReader = null;
+
+		public SQLCommandFactory(MySqlConnection mySqlConnection, MySqlTransaction mySqlTransaction)
+		{
+			this.mySqlConnection = mySqlConnection ?? throw new ArgumentNullException(nameof(mySqlConnection));
+			this.mySqlTransaction = mySqlTransaction ?? throw new ArgumentNullException(nameof(mySqlTransaction));
+		}
+
+		public MySqlDataReader SafeExecuteReader(MySqlCommand mySqlCommand){
+			StaticUtils.CheckSafety2(dataReader, "Data reader already created!");
+			MySqlDataReader temp = mySqlCommand.ExecuteReader();
+			dataReader = temp;
+			return temp;
+		}
+
+		public void SafeDestroyReader(){
+			StaticUtils.CheckSafety(dataReader, "Data reader already destroyed!");
+			dataReader.Dispose();
+			dataReader = null;
+		}
+
+		private void RequireTransaction()
+		{
+			StaticUtils.CheckSafety2(disposedValue, "MySQL connection already disposed!");
+			StaticUtils.CheckSafety(mySqlTransaction, "MySQL transaction not open!");
+		}
+
+		public MySqlCommand GetCommand(string cmd)
+		{
+			RequireTransaction();
+			return new MySqlCommand(cmd, mySqlConnection, mySqlTransaction);
+		}
+
+		public void FlushTransaction(bool commit)
+		{
+			RequireTransaction();
+			try
+			{
+				if (commit)
+				{
+					StaticUtils.CheckSafety2(dataReader, "Data reader still open!");
+					mySqlTransaction.Commit();
+				}
+				else
+				{
+					if(dataReader != null){
+						SafeDestroyReader();
+					}
+					mySqlTransaction.Rollback();
+				}
+
+				mySqlTransaction.Dispose();
+				mySqlTransaction = mySqlConnection.BeginTransaction();
+			}
+			catch
+			{
+				throw new SafetyException("Unable to flush MySQL transaction!");
+			}
+			StaticUtils.CheckSafety(mySqlConnection, "Unable to flush MySQL transaction!");
+		}
+
+		public void DestroyTransaction(bool commit, bool destroy)
+		{
+			RequireTransaction();
+			try
+			{
+				if (commit)
+				{
+					StaticUtils.CheckSafety2(dataReader, "Data reader still open!");
+					mySqlTransaction.Commit();
+				}
+				else
+				{
+					if (dataReader != null)
+					{
+						SafeDestroyReader();
+					}
+					mySqlTransaction.Rollback();
+				}
+
+				if (destroy)
+				{
+					Dispose();
+				}
+
+			}
+			catch (Exception e)
+			{
+				if (StaticUtils.debug)
+				{
+					throw e;
+				}
+				else
+				{
+					throw new SafetyException("Unable to destroy MySQL transaction!");
+				}
+
+			}
+		}
+
+		public void BeginTransaction()
+		{
+			StaticUtils.CheckSafety2(disposedValue, "MySQL connection already disposed!");
+			StaticUtils.CheckSafety2(mySqlTransaction, "MySQL transaction already exist!");
+			mySqlTransaction = mySqlConnection.BeginTransaction();
+		}
+
+		public void Dispose()
+		{
+			GC.SuppressFinalize(this);
+			if (!disposedValue)
+			{
+				if (mySqlTransaction != null)
+				{
+					DestroyTransaction(false, true);
+				}
+				else
+				{
+					mySqlConnection.Close();
+
+					// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+					// TODO: set large fields to null
+					disposedValue = true;
+				}
+			}
+		}
+
+		~SQLCommandFactory()
+		{
+			Dispose();
+		}
+	}
+}
